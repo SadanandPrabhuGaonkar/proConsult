113152d4c69f335f55a663929940aa4467347226
Nilesh <nilesh@tentwenty.me>
2023-04-11 02:39:40 +0530

    search functionality

--- application/page_templates/search_results.php
+++ application/page_templates/search_results.php
@@ -1,8 +1,12 @@
 <?php defined('C5_EXECUTE') or die("Access Denied.");
+use Concrete\Core\View\View;
+$th           = Loader::helper('text');
 
 $site = Config::get('concrete.site');
 $themePath = $this->getThemePath();
 $this->inc('includes/banner.php'); 
+$page = Page::getCurrentPage();
+$keywords     = $th->entities($th->sanitize($_GET['keywords']));
 
 ?>
 
@@ -11,10 +15,9 @@ $this->inc('includes/banner.php');
 <section class="results">
     <h3>Search</h3>
 <div class="search-btn-input">
-    <form action="">
-        <input type="text" name="keywords" placeholder="Type in your keyword">
-            <button>
-             Search
+    <form>
+    <input id="keywords" type="text" name="query" value="<?php echo $keywords;?>" placeholder="<?php echo t('What are you looking for?'); ?> "  autocomplete="off" >
+            <button> Search
             </button>
     </form>
 </div>
@@ -22,49 +25,18 @@ $this->inc('includes/banner.php');
 <div class="results-inner">
     <h3>Results</h3>
     <section class="services common-padding">
-   <div class="service-details">
-      <img src="/proConsult/application/themes/proConsult/dist/images/Union.png" alt="union" class="uni1">
-      <img src="/proConsult/application/themes/proConsult/dist/images/Union.png" alt="union" class="uni2">
-      <div class="service-card fadeup" style="translate: none; rotate: none; scale: none; transform: translate(0px, 0px); opacity: 1; visibility: inherit;">
-         <div class="image-card">
-            <a class="absul" href="http://localhost/proConsult/index.php/services/procurement-sourcing">Continue Reading</a>                        <img src="http://localhost/proConsult/application/files/3116/7999/9954/Sourcing-and-Procurement-in-SAP_1.jpg" alt="Sourcing-and-Procurement-in-SAP (1).jpg">            
-            <div class="content">
-               <p>We ensure that the client’s delivery strategy, performance and business terms are properly captured in contract that is realistic, achievable and meet or Exceeds clients businesses qualitative and Quantitative Key Performance Indicators(KPIs)</p>
-            </div>
-         </div>
-         <h4>Procurement &amp; Sourcing</h4>
-         <a href="http://localhost/proConsult/index.php/services/procurement-sourcing">Continue Reading</a>        
-      </div>
-      <div class="service-card fadeup" style="translate: none; rotate: none; scale: none; transform: translate(0px, 0px); opacity: 1; visibility: inherit;">
-         <div class="image-card">
-            <a class="absul" href="http://localhost/proConsult/index.php/services/quality-management-systems">Continue Reading</a>                        <img src="http://localhost/proConsult/application/files/4816/7999/9956/Management-Consultancy_1_1.jpg" alt="Management-Consultancy 1 (1).jpg">            
-            <div class="content">
-               <p>Design and build portions serve to develop the structure of a QMS, its processes, and plans for implementation.</p>
-            </div>
-         </div>
-         <h4>Quality Management Systems</h4>
-         <a href="http://localhost/proConsult/index.php/services/quality-management-systems">Continue Reading</a>        
-      </div>
-      <div class="service-card fadeup" style="translate: none; rotate: none; scale: none; transform: translate(0px, 0px); opacity: 1; visibility: inherit;">
-         <div class="image-card">
-            <a class="absul" href="http://localhost/proConsult/index.php/services/project-procurement-management">Continue Reading</a>                        <img src="http://localhost/proConsult/application/files/1116/8000/6667/pexels-lukas-590022_1_1_1_1.jpg" alt="pexels-lukas-590022 1 1 (1) (1).jpg">            
-            <div class="content">
-               <p>Our methodology, adaptability shall bridge the gaps of completeness to fulfilment of all challenges thereby increasing efficiency, improving productivity, surpassing margins and getting into the best books of your customers.</p>
-            </div>
-         </div>
-         <h4>Project Procurement Management</h4>
-         <a href="http://localhost/proConsult/index.php/services/project-procurement-management">Continue Reading</a>        
-      </div>
-      <div class="service-card fadeup" style="translate: none; rotate: none; scale: none; transform: translate(0px, 0px); opacity: 1; visibility: inherit;">
-         <div class="image-card">
-            <a class="absul" href="http://localhost/proConsult/index.php/services/e-transformation">Continue Reading</a>                        <img src="http://localhost/proConsult/application/files/4416/7999/9957/Digital_engeenearing_works_Banner_va2-04_1_1.png" alt="Digital engeenearing works Banner_va2-04 1 (1).png">            
-            <div class="content">
-               <p>Economic motive to be the primary driver for oganisation to  adopt e-procurements  systems and that motivation for implementation was based on expectations of lower purchase prices, reduced transaction costs, increased speed and efficiency..</p>
-            </div>
-         </div>
-         <h4>E-Transformation</h4>
-         <a href="http://localhost/proConsult/index.php/services/e-transformation">Continue Reading</a>        
-      </div>
+   
+    <div class="service-details search--list">
+    <img src="/proConsult/application/themes/proConsult/dist/images/Union.png" alt="union" class="uni1">
+    <img src="/proConsult/application/themes/proConsult/dist/images/Union.png" alt="union" class="uni2">
+      
+      <?php foreach ($searchResults as $searchResult) {
+              View::element('search', ['searchResult' => $searchResult, 'ih' => $ih, 'themePath' => $themePath]);
+        } ?>
+        <?php if (!$searchResults) { ?>
+          <h4 class="" style="margin-top:32px"><?php echo t('There were no results found. Please try another keyword or phrase.')?></h4>
+        <?php } ?>
+    
    </div>
 </section>
 </div>
---
+++ application/themes/proConsult/src/js/pages/Search.js
@@ -0,0 +1,162 @@
+
+export default class SearchListing {
+    constructor() {
+        this.document = $(document);
+        this.window = $(window);
+        this.grid = $('.search--list');
+        this.loader = $('.loader');
+        this.searchInput = $('#keywords');
+        this.searchIcon = $('.search-btn-input button');
+        this.keywords = $('#keywords');
+        this.loadBtn = $('.load--more');
+        this.page = 1;
+        this.bindEvents();
+    }
+
+    bindEvents = () => {
+        if (this.keywords) url.searchParams.append(this.name,this.value);
+        this.keywords.on('keyup', this.delayLoad);
+        this.loadBtn.on('click', this.loadMore);
+        // this.document.ready(this.checkKeyword);
+
+    };
+
+    // checkKeyword = () => {
+    //     if (!this.getKeywords()) {
+    //         this.hideLoader();
+    //         let data = `<div class="col-sm-12"><h3>Enter a keyword</h3></div>`;
+    //         this.grid.html(data);
+    //         return;
+    //     }
+    //     this.delayLoad();
+    // };
+    
+    delayLoad = (e) => {
+        console.log("comes")
+        e.preventDefault();
+        clearInterval(this.timer);
+        // this.scrollToListing();
+        this.hideLoadMore();
+        // setTimeout(() => {
+        //     this.grid.empty();
+        // }, 300);
+        // this.timer = setTimeout(() => this.filter(), 500);
+        this.filter()
+    };
+
+  
+
+    
+
+    filter = () => {
+        console.log(`${this.getCurrentPagePath()}`);
+        this.resetPage();
+        this.showLoader();
+        $.ajax({
+            type: 'GET',
+            data: {
+                keywords: this.getKeywords(),
+                page: this.getPage(),
+                isAjax: true
+            },
+            url: `${this.getCurrentPagePath()}`,
+            success: this.applyFilters,
+            complete: this.hideLoader()
+        });
+        this.setURL();
+    };
+
+    loadMore = () => {
+        this.addPage();
+        this.hideLoadMore();
+        this.showLoader();
+        $.ajax({
+            type: 'GET',
+            data: {
+                keywords: this.getKeywords(),
+                page: this.getPage(),
+                isAjax: true
+            },
+            url: `${this.getCurrentPagePath()}`,
+            success: this.appendItems,
+            complete: this.hideLoader()
+        });
+    };
+
+    setURL = () => {
+        const href = `${this.getCurrentPagePath()}?${$.param({
+            keywords: this.getKeywords(),
+        })}`;
+        window.history.pushState({ href: href }, '', href);
+    };
+
+    applyFilters = (response) => {
+        let data;
+        if (!response) {
+            data = `<div class="no-results-msg"><h4>No items match your filters.</h4></div>`;
+        } else {
+            data = $(response);
+            this.showLoadMore();
+        }
+       
+        this.grid.html(data);
+        this.grid.prepend(`
+        <img src="/proConsult/application/themes/proConsult/dist/images/Union.png" alt="union" class="uni1">
+        <img src="/proConsult/application/themes/proConsult/dist/images/Union.png" alt="union" class="uni2">
+          
+        `);
+        //  setTimeout(() => {
+        //     const observer = lozad();
+        //     observer.observe();
+        //   }, 500);
+    };
+
+    appendItems = (response) => {
+        let data;
+        if (response) {
+            data = $(response);
+            this.grid.append(data);
+            this.showLoadMore();
+        }
+        setTimeout(() => {
+            const observer = lozad(); // lazy loads 
+            observer.observe();
+          }, 500);
+    };
+
+    getKeywords = () => {
+        return this.keywords.val() ? encodeURI(this.keywords.val()) : '';
+    };
+
+    hideLoadMore = () => {
+        this.loadBtn.fadeOut();
+    };
+
+    showLoadMore = () => {
+        this.loadBtn.fadeIn();
+    };
+
+    hideLoader = () => {
+        this.loader.fadeOut();
+    };
+
+    showLoader = () => {
+        this.loader.fadeIn();
+    };
+
+    addPage = () => {
+        this.page += 1;
+    };
+
+    resetPage = () => {
+        this.page = 1;
+    };
+
+    getPage = () => {
+        return this.page;
+    };
+
+    getCurrentPagePath = () => {
+        return location.href.split('?')[0].split('#')[0];
+    };
+}
