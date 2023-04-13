ac97604ad02b771ea8c43b915b5e9b62734f2f55
Nilesh <nilesh@tentwenty.me>
2023-04-13 21:28:36 +0530

    placeholder added and search thumbnail image fix

--- application/elements/search.php
+++ application/elements/search.php
@@ -1,11 +1,14 @@
 <?php $title = $searchResult->getCollectionName();
 $url = $searchResult->getCollectionPath();
 $desc = $searchResult->getCollectionDescription();
+use Application\Concrete\Helpers\ImageHelper;
+$ih = new ImageHelper();
+$thumbnail = $searchResult->getAttribute('thumbnail_image');
 
 ?>
 <div class="service-card fadeup" style="translate: none; rotate: none; scale: none; transform: translate(0px, 0px); opacity: 1; visibility: inherit;">
   <div class="image-card">
-    <a class="absul" href="<?php echo $url; ?>">Continue Reading</a> <img src="http://localhost/proConsult/application/files/3116/7999/9954/Sourcing-and-Procurement-in-SAP_1.jpg" alt="Sourcing-and-Procurement-in-SAP (1).jpg">
+    <a class="absul" href="<?php echo $url; ?>">Continue Reading</a> <img src="<?php echo $ih->getThumbnail($thumbnail, 1980, 1980) ?>" alt="<?php echo $title; ?>">
     <div class="content">
       <p><?php echo $desc; ?></p>
     </div>
