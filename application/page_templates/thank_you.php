52860f8e09cf3b36294bd497394e097775b96f5e
Nilesh <nilesh@tentwenty.me>
2023-04-20 17:18:02 +0530

    union image fix

--- application/page_templates/thank_you.php
+++ application/page_templates/thank_you.php
@@ -1,7 +1,7 @@
 <section class="thank_you common-padding">
-    <img src="<?php echo $themePath; ?>/dist/images/union.png" alt="union" class="uni1"/>
-    <img src="<?php echo $themePath; ?>/dist/images/union.png" alt="union" class="uni2"/>
-    <img src="<?php echo $themePath; ?>/dist/images/union.png" alt="union" class="uni3"/>
+    <img src="<?php echo $themePath; ?>/dist/images/Union.png" alt="Union" class="uni1"/>
+    <img src="<?php echo $themePath; ?>/dist/images/Union.png" alt="Union" class="uni2"/>
+    <img src="<?php echo $themePath; ?>/dist/images/Union.png" alt="Union" class="uni3"/>
     <h1>Thank You For Contacting Us</h1>
     <p>Your submission has been recieved. Please allow upto 2 business days for our reply. Depending on the business request, it may take longer to get back to you.</p>
     <a href="<?php echo View::url('/'); ?>" class="btn-main btn-blue-background fadeuplate">Go back to homepage</a>
